a04b68be4a5e27f8991c9b287d4111e76b6f35ba
diego <diego.geoprocessamento@gmail.com>
2018-06-05 08:30:18 -0300

    05

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 
 // criar o express
 const app = express();
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000'); // verificar a porta
 app.set('port', port);
 
 // criar o servidor
@@ -25,6 +25,8 @@ app.use('/', route);
 
 // rodar a API
 server.listen(port);
+server.on('error', onError); // verificar erros
+
 console.log('API rodando na porta ' + port);
 
 // verificar a porta
@@ -37,5 +39,29 @@ function normalizePort(val){
 
     if(port >= 0){
         return port;
+    }
+}
+
+// verificar erros
+function onError(error){
+    if (error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code){
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
     }
 }
